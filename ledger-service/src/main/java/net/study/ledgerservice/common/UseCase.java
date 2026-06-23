21a2fce92214da6014a1975949826c7c9c803d9d
ksw <seonwongim@gmail.com>
2026-06-23 22:44:58 +0900

    공통 어노테이션에 @Component 어노테이션 추가

--- ledger-service/src/main/java/net/study/ledgerservice/common/UseCase.java
+++ ledger-service/src/main/java/net/study/ledgerservice/common/UseCase.java
@@ -1,8 +1,11 @@
 package net.study.ledgerservice.common;
 
+import org.springframework.stereotype.Component;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
+@Component
 @Target(ElementType.TYPE)
 public @interface UseCase {
 }
