9254e5523edf749dacc728f33b8f69a4fef1a410
ksw <seonwongim@gmail.com>
2026-06-23 23:53:56 +0900

    @PersistenceAdapter - 리포지토리 어댑터 추가

---
+++ ledger-service/src/main/java/net/study/ledgerservice/common/PersistenceAdapter.java
@@ -0,0 +1,11 @@
+package net.study.ledgerservice.common;
+
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Component
+@Target(ElementType.TYPE)
+public @interface PersistenceAdapter {
+}
