e7ed79e5a0714cf957f7c59388f61f1c60981dfe
ksw <seonwongim@gmail.com>
2026-06-24 16:03:28 +0900

    리토지토리 어노테이션 추가

--- ledger-service/src/main/java/net/study/ledgerservice/ledger/adapter/out/repository/JpaLedgerTransactionRepository.java
+++ ledger-service/src/main/java/net/study/ledgerservice/ledger/adapter/out/repository/JpaLedgerTransactionRepository.java
@@ -2,7 +2,9 @@ package net.study.ledgerservice.ledger.adapter.out.repository;
 
 import net.study.ledgerservice.ledger.adapter.out.entity.LedgerTransactionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface JpaLedgerTransactionRepository extends JpaRepository<LedgerTransactionEntity, Long> {
 
     Boolean isExistByOrderId(String orderId);
